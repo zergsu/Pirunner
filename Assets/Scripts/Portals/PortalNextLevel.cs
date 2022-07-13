73e93bfa43ea0d00f60bea1591add52082de84ef
Ellie2405 <mttne2405@gmail.com>
2022-07-13 18:48:52 +0300

    Update PortalNextLevel.cs

--- Assets/Scripts/Portals/PortalNextLevel.cs
+++ Assets/Scripts/Portals/PortalNextLevel.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class PortalNextLevel : MonoBehaviour
 {
     [SerializeField] GameObject twirlEffect;
-	[SerializeField] GameObject particleEffect;
-	[SerializeField] Timer timer;
+    [SerializeField] GameObject particleEffect;
+    [SerializeField] Timer timer;
 
-	private void Start()
-	{
-		twirlEffect.SetActive(false);
-	}
-
-	private void OnTriggerEnter(Collider other)
+    private void Start()
+    {
+        twirlEffect.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
             twirlEffect.SetActive(true);
-		    particleEffect.SetActive(true);
-		GameManager.instance.playerWon = true;
-	}
+            particleEffect.SetActive(true);
+
+            GameManager.instance.playerWon = true;
+            
+            Debug.Log(GameManager.instance.timer.gameTime);
+            SaveData.CheckScore(GameManager.instance.timer.gameTime);
+            SaveData.UnlockNextLevel();
+        }
+    }
 }
