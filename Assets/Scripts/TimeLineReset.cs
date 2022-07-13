be673af04dabea62b354eaafc78a757652ab27dc
zergsu <godofrock10@gmail.com>
2022-07-13 21:44:57 +0300

    added cutscenes and timeline manager

---
+++ Assets/Scripts/TimeLineReset.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimeLineReset : MonoBehaviour
+{
+    [SerializeField] GameObject camPos;
+    [SerializeField] GameObject fadeFX;
+	[SerializeField] GameObject Timer;
+	[SerializeField] PlayableDirector Timeline;
+
+	[SerializeField] Vector3 camPosReset = new Vector3(-6f, 6.5f, -16.6f);
+
+
+	private void Update()
+	{
+		if(Timeline.state !=PlayState.Playing)
+		{
+			camPos.transform.position = camPosReset;
+			camPos.transform.rotation = Quaternion.Euler(0, 0, 0);
+			Destroy(fadeFX);
+			Timer.SetActive(true);
+			Destroy(gameObject);
+		}
+	}
+}
